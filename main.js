82a31a72e4a8425a5c8e7e975e1067dcbd2bcd1e
shellchann <137977278+potato925@users.noreply.github.com>
2023-10-31 14:36:11 +0900

    上下に動かない

--- main.js
+++ main.js
@@ -1,63 +1,63 @@
-var canvas = document.getElementById("gameCanvas");
-var ctx = canvas.getContext("2d");
-var boxSize = 20;
-var canvasSize = canvas.width;
-var snake = [{ x: 10, y: 10 }];
-var food = generateFoodPosition();
-var dx = 0;
-var dy = 0;
-function generateFoodPosition() {
-    var x = Math.floor(Math.random() * (canvasSize / boxSize)) * boxSize;
-    var y = Math.floor(Math.random() * (canvasSize / boxSize)) * boxSize;
-    return { x: x, y: y };
-}
-function drawSnake() {
-    snake.forEach(function (segment) {
-        ctx.fillStyle = "green";
-        ctx.fillRect(segment.x, segment.y, boxSize, boxSize);
-    });
-}
-function drawFood() {
-    ctx.fillStyle = "red";
-    ctx.fillRect(food.x, food.y, boxSize, boxSize);
-}
-function moveSnake() {
-    var head = { x: snake[0].x + dx, y: snake[0].y + dy };
-    snake.unshift(head);
-    if (head.x === food.x && head.y === food.y) {
-        food = generateFoodPosition();
+var canvas = document.createElement('canvas');
+var ctx = canvas.getContext('2d');
+canvas.width = 400;
+canvas.height = 400;
+canvas.setAttribute('style', 'display:block:margin:auto;background-color: #aaa');
+document.body.appendChild(canvas);
+var GRID = 20;
+var STAGE = canvas.width / GRID;
+var snake = {
+    x: null,
+    y: null,
+    dx: 1,
+    dy: 0,
+    tail: null,
+    update: function () {
+        this.body.push({ x: this.x, y: this.y });
+        this.x += this.dx;
+        this.y += this.dy;
+        ctx.fillStyle = 'green';
+        this.body.forEach(function (obj) {
+            ctx === null || ctx === void 0 ? void 0 : ctx.fillRect(obj.x * GRID, obj.y * GRID, GRID - 2, GRID - 2);
+        });
+        if (this.body.length > this.tail)
+            this.body.shift();
     }
-    else {
-        snake.pop();
-    }
-}
-function clearCanvas() {
-    ctx.clearRect(0, 0, canvasSize, canvasSize);
-}
-function draw() {
-    clearCanvas();
-    moveSnake();
-    drawSnake();
-    drawFood();
-    requestAnimationFrame(draw);
-}
-document.addEventListener("keydown", function (event) {
-    var keyCode = event.keyCode;
-    if (keyCode === 37 && dx !== boxSize) {
-        dx = -boxSize;
-        dy = 0;
-    }
-    else if (keyCode === 39 && dx !== -boxSize) {
-        dx = boxSize;
-        dy = 0;
-    }
-    else if (keyCode === 38 && dy !== boxSize) {
-        dx = 0;
-        dy = -boxSize;
-    }
-    else if (keyCode === 40 && dy !== -boxSize) {
-        dx = 0;
-        dy = boxSize;
+};
+var item = {};
+var init = function () {
+    snake.x = STAGE / 2;
+    snake.y = STAGE / 2;
+    snake.tail = 4;
+    snake.body = [];
+};
+var loop = function () {
+    ctx === null || ctx === void 0 ? void 0 : ctx.clearRect(0, 0, canvas.width, canvas.height);
+    snake.update();
+};
+init();
+setInterval(loop, 1000 / 15);
+document.addEventListener('keydown', function (e) {
+    switch (e.key) {
+        case 'ArrowLeft':
+            ;
+            snake.dx = -1;
+            snake.by = 0;
+            break;
+        case 'ArrowRight':
+            ;
+            snake.dx = 1;
+            snake.by = 0;
+            break;
+        case 'ArrowUp':
+            ;
+            snake.dx = 0;
+            snake.by = -1;
+            break;
+        case 'ArrowDown':
+            ;
+            snake.dx = 0;
+            snake.by = 1;
+            break;
     }
 });
-draw();
--- main.ts
+++ main.ts
@@ -1,68 +1,66 @@
-const canvas = <HTMLCanvasElement>document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
 
-const boxSize = 20;
-const canvasSize = canvas.width;
-let snake: { x: number; y: number }[] = [{ x: 10, y: 10 }];
-let food: { x: number; y: number } = generateFoodPosition();
-let dx = 0;
-let dy = 0;
+canvas.width = 400;
+canvas.height = 400;
 
-function generateFoodPosition(): { x: number; y: number } {
-    const x = Math.floor(Math.random() * (canvasSize / boxSize)) * boxSize;
-    const y = Math.floor(Math.random() * (canvasSize / boxSize)) * boxSize;
-    return { x, y };
+canvas.setAttribute('style', 'display:block:margin:auto;background-color: #aaa');
+
+document.body.appendChild(canvas);
+
+const GRID = 20;
+const STAGE = canvas.width / GRID;
+
+const snake = {
+  x: null,
+  y: null,
+  dx: 1,
+  dy: 0,
+  tail: null,
+
+  update: function() {
+    this.body.push({x: this.x, y: this.y});
+    this.x += this.dx;
+    this.y += this.dy;
+
+    ctx.fillStyle = 'green';
+
+    this.body.forEach(obj => {
+      ctx?.fillRect(obj.x * GRID, obj.y * GRID, GRID-2, GRID-2);
+    })
+    if(this.body.length > this.tail) this.body.shift();
+  }
+}
+const item = {}
+
+const init = () => {
+  snake.x = STAGE / 2;
+  snake.y = STAGE / 2;
+  snake.tail = 4;
+  snake.body = [];
+}
+const loop = () => {
+  ctx?.clearRect(0,0,canvas.width,canvas.height);
+
+  snake.update();
 }
 
-function drawSnake() {
-    snake.forEach(segment => {
-        ctx.fillStyle = "green";
-        ctx.fillRect(segment.x, segment.y, boxSize, boxSize);
-    });
-}
+init();
+setInterval(loop, 1000/15);
 
-function drawFood() {
-    ctx.fillStyle = "red";
-    ctx.fillRect(food.x, food.y, boxSize, boxSize);
-}
-
-function moveSnake() {
-    const head = { x: snake[0].x + dx, y: snake[0].y + dy };
-    snake.unshift(head);
-    if (head.x === food.x && head.y === food.y) {
-        food = generateFoodPosition();
-    } else {
-        snake.pop();
-    }
-}
-
-function clearCanvas() {
-    ctx.clearRect(0, 0, canvasSize, canvasSize);
-}
-
-function draw() {
-    clearCanvas();
-    moveSnake();
-    drawSnake();
-    drawFood();
-    requestAnimationFrame(draw);
-}
-
-document.addEventListener("keydown", (event) => {
-    const keyCode = event.keyCode;
-    if (keyCode === 37 && dx !== boxSize) {
-        dx = -boxSize;
-        dy = 0;
-    } else if (keyCode === 39 && dx !== -boxSize) {
-        dx = boxSize;
-        dy = 0;
-    } else if (keyCode === 38 && dy !== boxSize) {
-        dx = 0;
-        dy = -boxSize;
-    } else if (keyCode === 40 && dy !== -boxSize) {
-        dx = 0;
-        dy = boxSize;
-    }
-});
-
-draw();
+document.addEventListener('keydown', e => {
+  switch(e.key){
+    case 'ArrowLeft';
+    snake.dx = -1;snake.by = 0;
+    break;
+    case 'ArrowRight';
+    snake.dx = 1;snake.by = 0;
+    break;
+    case 'ArrowUp';
+    snake.dx = 0;snake.by = -1;
+    break;
+    case 'ArrowDown';
+    snake.dx = 0;snake.by = 1;
+    break;
+  }
+} );
